02258537d0ccc564dada6d03e585d917c2ef032a
sampurnag <sampurnagadipudi@gmail.com>
2019-10-21 19:26:22 +0530

    added changes in testrunner.java

--- FreeCrmBDDFramework/src/main/java/myRunner/TestRunner.java
+++ FreeCrmBDDFramework/src/main/java/myRunner/TestRunner.java
@@ -7,7 +7,9 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features= {"\\Users\\Sampurna_Gadipudi\\eclipse-workspace\\FreeCrmBDDFramework\\src\\main\\java\\Features\\login.feature"}
-,glue={"stepDefinations"}
+,glue={"stepDefinations"},
+monochrome=true,
+dryRun=false
 
 		)
 public class TestRunner {
